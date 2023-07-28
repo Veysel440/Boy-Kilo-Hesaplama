32370441aa5086c681cfaf50379102d8ccfe9ec2
Veysel440 <109949012+Veysel440@users.noreply.github.com>
2023-07-28 19:50:46 +0300

    Initial commit

---
+++ Boy Kilo hesaplama/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace vkivucutkitleindeksi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_hesapla = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_hesapla
+            // 
+            this.button_hesapla.ForeColor = System.Drawing.Color.DarkBlue;
+            this.button_hesapla.Location = new System.Drawing.Point(271, 149);
+            this.button_hesapla.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button_hesapla.Name = "button_hesapla";
+            this.button_hesapla.Size = new System.Drawing.Size(112, 37);
+            this.button_hesapla.TabIndex = 0;
+            this.button_hesapla.Text = "Hesapla";
+            this.button_hesapla.UseVisualStyleBackColor = true;
+            this.button_hesapla.Click += new System.EventHandler(this.Button_hesapla_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Boyunuz (CM) :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(75, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Kilonuz (KG) :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(182, 76);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 29);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "160";
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.sadeceSayiGirme);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(183, 109);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(200, 29);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "55";
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.sadeceSayiGirme);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(61, 206);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 21);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Sonuç";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::vkivucutkitleindeksi.Properties.Resources.proje_ve_kod_projevekod_csharp_projeleri_oyunlari_dersleri_ornekleri_small_logo_2;
+            this.pictureBox1.Location = new System.Drawing.Point(155, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(192, 58);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.ClientSize = new System.Drawing.Size(477, 273);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_hesapla);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ForeColor = System.Drawing.Color.Cornsilk;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VKİ Vücut Kitle İndeksi Hesaplama - Proje ve Kod Paylaşım Platformu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_hesapla;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Boy Kilo hesaplama/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vkivucutkitleindeksi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void sadeceSayiGirme(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void Button_hesapla_Click(object sender, EventArgs e)
+        {
+            double boy = Convert.ToDouble(textBox1.Text.Insert(1,","));
+            int kg = Convert.ToInt32(textBox2.Text);
+            int sonuc = Convert.ToInt32(kg / (boy * boy));
+            if (sonuc > 15 && sonuc < 20) label3.Text = "Biraz zayıfsınız. Beslenmenizi arttırmanız gerekli";
+            if (sonuc > 20 && sonuc < 25) label3.Text = "İdeal Kilodasınız";
+            if (sonuc > 25 && sonuc < 30) label3.Text = "Fazla kilonuz bulunuyor. Beslenmenizi arttırmayınız.";
+            if (sonuc > 30 && sonuc < 40) label3.Text = "Obezsiniz. Beslenmenize dikkat etmelisiniz.";
+            if (sonuc > 40) label3.Text = "Aşırı obezsiniz. Beslenmenize dikkat etmelisiniz.";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
